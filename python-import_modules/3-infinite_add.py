0480cc3c3ac44273dfb7c4e009843e1fbb68dfa5
root <JaggerVanW@gmail.com>
2023-09-06 22:41:10 -0700

    Adding ARGS

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    for i in range (0, len(argv)):
-        print(i)
+    sum = 0
+    for i in argv[1:]:
+        sum += int(i)
+    print(sum)
