bc2321d91719a646570bb9d7e9e8b18692df9a37
root <JaggerVanW@gmail.com>
2023-09-09 12:43:27 -0700

    Index check

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
-    for x in my_list:
-        if idx == my_list[x]:
-            print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    else:
+        return my_list[idx]
