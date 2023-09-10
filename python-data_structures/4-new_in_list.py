df32024a8018177547f04c4fc096059fbcadff82
root <JaggerVanW@gmail.com>
2023-09-10 16:14:26 -0700

    Test

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,5 +3,8 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        my_list[idx] = element
-        return my_list
+        new_list = my_list
+        new_list[idx] = element
+        return new_list
+print(new_in_list([1, 2, 3, 4, 5], 3, 9))
+print(new_in_list([1, 2, 3, 4, 5], -10, 7))
