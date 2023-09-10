f1b8a02c21cbf177c03ff5b09b8a5911b3e28522
root <JaggerVanW@gmail.com>
2023-09-10 16:15:42 -0700

    Test

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,5 +6,3 @@ def new_in_list(my_list, idx, element):
         new_list = my_list
         new_list[idx] = element
         return new_list
-print(new_in_list([1, 2, 3, 4, 5], 3, 9))
-print(new_in_list([1, 2, 3, 4, 5], -10, 7))
