57ddd164f9dca4ea4474d1c4203b9a796f5de695
root <JaggerVanW@gmail.com>
2023-09-10 16:58:34 -0700

    Removing Cc's

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     string_list = list(my_string)
     for index in range(len(string_list)):
         if string_list[index] == 'c' or string_list[index] == 'C':
-            string_list[index] = ''
+            string_list[index] = None
     new_string = "".join(string_list)
     return new_string
 print(no_c("See is not Cee or Cese"))
