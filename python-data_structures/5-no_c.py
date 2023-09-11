8a08707d6e145301be2623648e3f0a1a2b6c026d
root <JaggerVanW@gmail.com>
2023-09-10 17:07:52 -0700

    Remove Cc's

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    string_list = list(my_string)
-    for index in range(len(string_list)):
-        if string_list[index] == 'c' or string_list[index] == 'C':
-            string_list[index] = None
-    new_string = "".join(string_list)
+    remove_c = {67:None, 99:None}
+    new_string = my_string.translate(remove_c)
     return new_string
-print(no_c("See is not Cee or Cese"))
