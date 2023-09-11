16bfc34b65d5263ea1c2b5a9aa6eb3a09a6a53ee
root <JaggerVanW@gmail.com>
2023-09-10 17:14:12 -0700

    Test

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    remove_c = {67:None, 99:None}
+    remove_c = {67: None, 99: None}
     new_string = my_string.translate(remove_c)
     return new_string
