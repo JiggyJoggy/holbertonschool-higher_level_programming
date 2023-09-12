930a510f7be688eeaf1e9f92f25305d17cffdaf4
root <JaggerVanW@gmail.com>
2023-09-12 14:21:30 -0700

    Fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@ def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-            'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
-            'CD': 400, 'CM': 900}
+             'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
+             'CD': 400, 'CM': 900}
     i = 0
     number = 0
     while i < len(roman_string):
