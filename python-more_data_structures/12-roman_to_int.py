03f36fb25e4448f2609902d1ef512f7f57e232f1
root <JaggerVanW@gmail.com>
2023-09-12 14:17:05 -0700

    Roman ints

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,13 +5,13 @@ def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
             'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
             'CD': 400, 'CM': 900}
-    index = 0
+    idx = 0
     number = 0
-    while index < len(roman_string):
-        if index + 1 < len(roman_string) and roman_string[index:index + 2] in roman:
-            number += roman[roman_string[index:index + 2]]
-            index += 2
+    while idx < len(roman_string):
+        if idx + 1 < len(roman_string) and roman_string[idx:idx + 2] in roman:
+            number += roman[roman_string[idx:idx + 2]]
+            idx += 2
         else:
-            number += roman[roman_string[index]]
-            index += 1
+            number += roman[roman_string[idx]]
+            idx += 1
     return number
