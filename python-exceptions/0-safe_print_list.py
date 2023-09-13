c250588ff6258902ca3defb2ef1bfe3d7548296f
root <JaggerVanW@gmail.com>
2023-09-12 18:43:24 -0700

    rage..

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     element = 0
-    for index in rage(x):
+    for index in range(x):
         try:
             print(my_list[index], end="")
             element += 1
