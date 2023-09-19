2575e784cf0fd68c4f2c562ef7ed87f7fabdd238
root <JaggerVanW@gmail.com>
2023-09-19 11:04:52 -0700

    Test

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Prints a square with the character #"""
+
+def print_square(size):
+    """Function that prints out the square using the character #"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif not isinstance(size, float) and size < 0:
+        else:
+            for row in range(size):
+                for col in range(size):
+                    print("#", end="")
+                print()
