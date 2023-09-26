6d100176ac742c08efd32cf74210453c87937cbc
root <JaggerVanW@gmail.com>
2023-09-25 21:34:08 -0700

    Object from a JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Creates an object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """Object from json file/filename"""
+    with open (filename, "r", encoding="utf-8") as f:
+        return json.loads(f.read())
