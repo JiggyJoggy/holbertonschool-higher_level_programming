4e727d6709df2d89976e920c13e9ac4c80f5480f
root <JaggerVanW@gmail.com>
2023-09-26 19:03:54 -0700

    Test

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """Class"""
+    def __init__(self, first_name, last_name, age):
+        """Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return dict rep"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            json_data = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    json_data[attr] = getattr(self, attr)
+            return json_data
