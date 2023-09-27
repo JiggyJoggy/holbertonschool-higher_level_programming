a71e2bb425c3e67f4e736289278836e8518d5ab9
root <JaggerVanW@gmail.com>
2023-09-26 19:08:37 -0700

    Test

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """Class"""
+    def __init__(self, first_name, last_name, age):
+        """Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dict rep"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            json_data = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    json_data[attr] = getattr(self, attr)
+            return json_data
+
+    def reload_from_json(self, json):
+        """Replace"""
+        for key, value in json.items():
+            self.__dict__[key] = value
