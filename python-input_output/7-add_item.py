29bdcb5f7fde67ebbde9e5cdd45432cc9a0aa6a5
root <JaggerVanW@gmail.com>
2023-09-26 18:48:39 -0700

    Test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """Adds items to a files"""
-import sys
+from sys import argv
+import json
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 
-if __name__ == '__main__':
-    save_to_json_file = \
-        __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
-    try:
-        list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        list = []
-    save_to_json_file(list + sys.argv[1:], "add_item.json")
+try:
+    mylist = load('add_item.json')
+except FileNotFoundError:
+    mylist = []
+finally:
+    save(mylist + argv[1:], 'add_item.json')
