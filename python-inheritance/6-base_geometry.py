b9fe8cbb1fa128a2f4a295621dd6eb60dd81079a
root <JaggerVanW@gmail.com>
2023-09-26 18:10:30 -0700

    Test

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """Class"""
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
