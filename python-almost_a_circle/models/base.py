5fd04959cc10740ddee0c60ad5288499f10be807
JiggyJoggy <JaggerVanW@gmail.com>
2023-09-29 16:25:37 -0500

    fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,11 +4,12 @@
 
 __nb_objects=0
 
+
 class Base():
     """Base class"""
     def __init__(self, id=None):
         """Checking id"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             __nb_objects + 1
