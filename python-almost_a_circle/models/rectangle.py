5dd73e4eb8f08500f078a4580d0e4ddb278bcbb1
JiggyJoggy <JaggerVanW@gmail.com>
2023-09-29 20:04:25 -0500

    fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,10 +8,10 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(self)
-        __width = width
-        __height = height
-        __x = x
-        __y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
