ddbbb045308ca5705044bed9d3cb5e32d721d9f2
JiggyJoggy <JaggerVanW@gmail.com>
2023-09-29 21:23:13 -0500

    long line fixed

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -84,4 +84,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns rectangle info"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".\
+            format(self.id, self.x, self.y, self.width, self.height)
