876b70c0b4f8d899ddbe1052cedc42a04ad6f7f9
JiggyJoggy <JaggerVanW@gmail.com>
2023-09-29 21:06:41 -0500

    test

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,9 @@ class Rectangle(Base):
     def area(self):
         """Getting area"""
         return self.__width * self.__height
+    
+    def display(self):
+        """Displays rectangle"""
+        for row in self.width:
+            for col in self.height:
+                print("#", end="")
