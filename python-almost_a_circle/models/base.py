d7a622c42b3462e6702ecbd8d57c526fc7564125
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-01 15:45:31 -0500

    return

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -36,5 +36,5 @@ class Base():
     @staticmethod
     def from_json_string(json_string):
         if json_string is None or len(json_string) == 0:
-            json_string = []
+            return []
         return json.loads(json_string)
