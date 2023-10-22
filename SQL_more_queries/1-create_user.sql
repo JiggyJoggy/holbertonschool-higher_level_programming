c8c66542ec1d064cd20121c8483c943991055b2e
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-22 17:35:57 -0500

    test

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Creates server user user_0d_1
 -- with all privileges
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' WITH PASSWORD 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
