68244a6a6a094154ba99854900ed075809f76f54
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-22 19:30:42 -0500

    testestest

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creates the database hbtn_0d_usa and the table states
+-- id is UNIQUE, AUTO GEN, NOT NULL (& name), and id is primary key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
