0e0325d55e3a88d566a253f0e6ad0a7e178a8fae
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-23 20:20:13 -0500

    ttt

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,5 +4,6 @@ SELECT tv_genres.name AS 'genre',
 COUNT(tv_shows.id) AS 'number_of_shows'
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NOT NULL
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
