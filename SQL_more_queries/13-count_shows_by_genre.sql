85c0136a0646ce79909cc41faf2f205beb189bea
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-23 19:55:25 -0500

    tetsetst

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all genres from hbtn_0d_tvshows
+-- and displays the number of shows linked to each
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
