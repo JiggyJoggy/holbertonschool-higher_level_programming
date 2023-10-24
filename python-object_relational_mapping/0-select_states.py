3ab12f44f74a18b310d1c04a58771534d65d3193
JiggyJoggy <JaggerVanW@gmail.com>
2023-10-24 16:24:53 -0500

    removed quotes

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,9 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host='localhost',
-        port='3306',
-        user='argv[1]',
-        password='argv[2]',
+        port=3306,
+        user=argv[1],
+        password=argv[2],
         db=argv[3]
     )
     cursor = db.cursor()
