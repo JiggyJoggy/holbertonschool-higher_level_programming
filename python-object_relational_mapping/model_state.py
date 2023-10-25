32e65b9913dcd20f22cfac6c755e7de7e36b5b35
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-10-24 23:33:25 -0500

    more spaces

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,9 +2,9 @@
 """class definition of a State and an instance Base = declarative_base()"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-
 Base = declarative_base()
+
+
 class State(Base):
     """State class"""
     __tablename__ = 'states'
