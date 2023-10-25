ec916b0dddcd8f7c39715cda0a8831e4a9d76176
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-10-25 00:24:21 -0500

    test

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-""" Script that adds state louisiana
-to database hbtn_0e_6_usa"""
+"""model_city.py that contains the class definition of a City"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
-
 if __name__ == "__main__":
     engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}\
                            @localhost:3306/{sys.argv[3]}', pool_pre_ping=True)
-
     Base.metadata.create_all(engine)
     session_maker = sessionmaker(bind=engine)
     session = session_maker()
