455bf66c4960cc451e63348dff54ac8b369d1cc1
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-10-24 23:58:38 -0500

    tttt

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}\
                            @localhost:3306/{sys.argv[3]}', pool_pre_ping=True)
