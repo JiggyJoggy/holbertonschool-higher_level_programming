c8e7fc7a26e1a34465dd0bc0e5c8325d343585ec
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-23 03:12:16 -0600

    check

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function add(a, b) {
-    a = parseInt(process.argv[2]);
-    b = parseInt(process.argv[3]);
+function add (a, b) {
+  a = parseInt(process.argv[2]);
+  b = parseInt(process.argv[3]);
 
-    if (Number(a) && Number(b)) {
-        console.log(`${(a + b)}`)
-    }
+  if (Number(a) && Number(b)) {
+    console.log(`${(a + b)}`);
+  }
 }
