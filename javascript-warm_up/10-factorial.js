37991e3c5e93bc25fd932770b7c7429a6ddef18b
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-23 04:24:47 -0600

    task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argInt = (parseInt(process.argv[2]));
+function factorial (num) {
+  if (isNaN(num) || num === 1 || num === 0) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
+console.log(factorial(argInt));
