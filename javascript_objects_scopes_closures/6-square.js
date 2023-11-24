5e19c65ab8b0300d64dcb95e146653764be6fae8
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-24 00:04:10 -0600

    whoops

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -4,15 +4,14 @@ const SquareV2 = require('./5-square');
 module.exports = class Square extends SquareV2 {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
-    for (let row = 0; row < this.size; row++) {
-      for (let col = 0; col < this.size; col++) {
+    for (let row = 0; row < this.width; row++) {
+      for (let col = 0; col < this.height; col++) {
         process.stdout.write(c);
       }
       process.stdout.write('\n');
