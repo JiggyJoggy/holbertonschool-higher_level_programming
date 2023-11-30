0a7096d279f5eac9def90f64391fb5d4a0d624f1
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-30 14:33:55 -0600

    task 2 dumb fix

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 const args = process.argv;
 const request = require('request');
 
-request(`${args[2]}`, (response) => {
+request.get(`${args[2]}`, function(error, response, body) {
     console.log('code: ', response.statusCode);
 })
