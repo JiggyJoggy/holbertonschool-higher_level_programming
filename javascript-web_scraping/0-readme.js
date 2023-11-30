f2c3e3883fdaf3370f489f30c5269aee7b23f5c5
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-30 13:28:54 -0600

    semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
-const fs = require('fs')
+const fs = require('fs');
 
 fs.readFile(`${args[2]}`, 'utf-8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-})
+  if (err) throw err;
+  console.log(data);
+});
