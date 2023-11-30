7b47b6060af2e9556468fba844ae0f556bb3d7e3
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-30 14:39:11 -0600

    overlooked semi

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,8 @@ const args = process.argv;
 const request = require('request');
 
 request.get(`${args[2]}`, function (error, response, body) {
+  if (error) {
+    throw error;
+  }
   console.log('code:', response.statusCode);
 });
