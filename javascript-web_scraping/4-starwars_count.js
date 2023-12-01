47256b19b1b1113b77df85c46c5825fd1506421b
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-12-01 14:23:05 -0600

    task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -11,8 +11,8 @@ request.get(`${arg[2]}`, function (error, response, body) {
   for (const film in films) {
     const char = films[film].characters;
     for (const person in char) {
-      if (char[person] === 'https://swapi-api.hbtn.io/api/people/18/') {
-        count++;
+      if (char[person].match(/.\/18\//g)) {
+				count++;
       }
     }
   }
